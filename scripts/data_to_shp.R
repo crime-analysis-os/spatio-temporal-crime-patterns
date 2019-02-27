69b287b10fa77a9c658b7649a24e81770fd84dfd
crime-analysis-os <hector.tundidor536@comunidadunir.net>
2019-02-27 19:18:12 +0100

    Create data_to_shp.R

---
+++ scripts/data_to_shp.R
@@ -0,0 +1,19 @@
+### Función para exportar un conjunto de datos a formato Shapefile
+
+data_to_shp <- function(datos, 
+                        name_outfile = "chicago_subset",
+                        crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs") {
+  
+  # Identificación de las coordenadas para crear un objeto espacial
+  coordinates(datos) <- ~Longitude+Latitude
+  
+  # Se establece el Sistema de Referencia de Coordenadas WGS84 y la proyección (CRS = 4326)
+  proj4string(datos) = CRS(crs)
+  
+  # Se comprueba la clase del objeto espacial
+  class(datos)
+  
+  # Creación del fichero con las geometrías vectoriales en formato Shapefile
+  writeOGR(datos, dsn = "../scripts", layer= name_outfile, driver = "ESRI Shapefile")
+  
+}
